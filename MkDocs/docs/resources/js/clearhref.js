578f33e7bcba5005fae0304032f8a50470d7afe5
zetaloop <zetaloop@outlook.com>
2024-06-06 14:52:27 +0800

    更新 clearhref.js 最完美的版本

--- MkDocs/docs/resources/js/clearhref.js
+++ MkDocs/docs/resources/js/clearhref.js
@@ -2,8 +2,11 @@ document$.subscribe(function () {
     var links = document.querySelectorAll("a");
     links.forEach(function (link) {
         if (!link.getAttribute("href")) {
-            link.setAttribute("href", "javascript:''.一个空白的链接");
+            link.removeAttribute("href");
             link.style.cursor = "auto";
+            link.addEventListener("click", function (e) {
+                e.stopImmediatePropagation();
+            });
         }
     });
 });
