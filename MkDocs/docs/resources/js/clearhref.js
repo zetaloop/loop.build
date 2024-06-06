0bfb96b8149fe2d1c51fb15141260a7a57d2aa67
zetaloop <zetaloop@outlook.com>
2024-06-06 14:46:08 +0800

    更新 clearhref.js

--- MkDocs/docs/resources/js/clearhref.js
+++ MkDocs/docs/resources/js/clearhref.js
@@ -2,7 +2,8 @@ document$.subscribe(function () {
     var links = document.querySelectorAll("a");
     links.forEach(function (link) {
         if (!link.getAttribute("href")) {
-            link.setAttribute("href", "javascript:''.什么都不会发生");
+            link.setAttribute("href", "javascript:''.一个空白的链接");
+            link.style.cursor = "auto";
         }
     });
 });
