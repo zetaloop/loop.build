34222786030680820f78bd1d66c1802710c0ab00
zetaloop <zetaloop@outlook.com>
2024-06-06 14:36:21 +0800

    更新 clearhref.js

--- MkDocs/docs/resources/js/clearhref.js
+++ MkDocs/docs/resources/js/clearhref.js
@@ -2,10 +2,7 @@ document$.subscribe(function () {
     var links = document.querySelectorAll("a");
     links.forEach(function (link) {
         if (!link.getAttribute("href")) {
-            link.setAttribute(
-                "href",
-                "javascript:'>>>'.点击这个链接什么都不会发生"
-            );
+            link.setAttribute("href", "javascript:''.什么都不会发生");
         }
     });
 });
