c4ef6c3ea8ade14bd978520a2af13519d5124bf7
zetaloop <zetaloop@outlook.com>
2024-06-19 19:21:45 +0800

    脚本添加更多提示

--- MkDocs/Make_Venv.bat
+++ MkDocs/Make_Venv.bat
@@ -1,11 +1,22 @@
+@echo off
+
+if exist venv (
+  echo [ERROR] venv already exists
+  pause
+  exit
+)
+
+if "%MATERIAL_INSIDERS_GH_TOKEN%"=="" (
+  echo [WARNING] MATERIAL_INSIDERS_GH_TOKEN is empty
+  pause
+)
+
 py -m venv venv
 call venv\Scripts\activate.bat
 pip install -U pip setuptools wheel
 pip install mkdocs-material[imaging] mkdocs-git-revision-date-localized-plugin
 pip install mkdocs-glightbox mkdocs-redirects mkdocs-minify-plugin
-if "%MATERIAL_INSIDERS_GH_TOKEN%"=="" (
-  echo "[WARNING] MATERIAL_INSIDERS_GH_TOKEN is empty"
-) else (
+if not "%MATERIAL_INSIDERS_GH_TOKEN%" == "" (
   pip install git+https://%MATERIAL_INSIDERS_GH_TOKEN%@github.com/squidfunk/mkdocs-material-insiders.git
 )
 cd ..\loop_custom
--- MkDocs/Make_Venv.sh
+++ MkDocs/Make_Venv.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+if [ -d "venv" ]; then
+  echo "[ERROR] venv already exists"
+  exit
+fi
+
+if [ -z "$MATERIAL_INSIDERS_GH_TOKEN" ]; then
+  echo "[WARNING] MATERIAL_INSIDERS_GH_TOKEN is empty"
+fi
+
 python3 -m venv venv
 source venv/bin/activate
 pip install -U pip setuptools wheel
 pip install mkdocs-material[imaging] mkdocs-git-revision-date-localized-plugin
 pip install mkdocs-glightbox mkdocs-redirects mkdocs-minify-plugin
-if [ -z "$MATERIAL_INSIDERS_GH_TOKEN" ]; then
-  echo "[WARNING] MATERIAL_INSIDERS_GH_TOKEN is empty"
-else
+if [ -n "$MATERIAL_INSIDERS_GH_TOKEN" ]; then
   pip install git+https://${MATERIAL_INSIDERS_GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git
 fi
 cd ../loop_custom
