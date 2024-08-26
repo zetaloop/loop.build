2a09c394c6f59f7380fb0ad9b1471b47873d8444
zetaloop <zetaloop@outlook.com>
2024-08-26 20:56:30 +0800

    现在可以点击输出消息了

--- MkDocs/docs/resources/js/betterHref.js
+++ MkDocs/docs/resources/js/betterHref.js
@@ -1,10 +1,25 @@
 document$.subscribe(function () {
-    var links = document.querySelectorAll("a");
-    links.forEach(function (link) {
-        if (!link.getAttribute("href")) {
-            link.removeAttribute("href");
-            link.style.cursor = "auto";
-            link.addEventListener("click", function (e) {
+    $("a").each(function () {
+        var $link = $(this);
+        if ($link.hasClass("msg")) {
+            msg = decodeURIComponent($link.attr("href"));
+            if (msg.startsWith("msg:")) {
+                msg = msg.substring(4);
+            } else if (msg.startsWith("base64:")) {
+                msg = atob(msg.substring(7));
+            }
+            if (msg.startsWith("'") && msg.endsWith("'")) {
+                msg = msg.substring(1, msg.length - 1);
+            }
+            $link.removeAttr("href").attr("hrefmsg", msg);
+            $link.on("click", function (e) {
+                e.stopImmediatePropagation();
+                alert$.next($link.attr("hrefmsg"));
+            });
+        } else if (!$link.attr("href")) {
+            $link.removeAttr("href");
+            $link.css("cursor", "auto");
+            $link.on("click", function (e) {
                 e.stopImmediatePropagation();
             });
         }
